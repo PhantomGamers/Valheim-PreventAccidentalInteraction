45b70bb2c2efa97729a11725258a541a76a819fa
Christopher Yeninas <phantom@sundergaming.com>
2021-03-03 22:18:53 -0500

    Make classes that are patched user configurable

--- PreventAccidentalInteraction/PreventAccidentalInteraction.cs
+++ PreventAccidentalInteraction/PreventAccidentalInteraction.cs
@@ -1,25 +1,42 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 
 using HarmonyLib;
 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
 namespace PreventAccidentalInteraction
 {
-    [BepInPlugin("com.github.phantomgamers.ValheimPreventAccidentalInteraction", "PreventAccidentalInteraction", "1.0.2")]
+    [BepInPlugin("com.github.phantomgamers.ValheimPreventAccidentalInteraction", "PreventAccidentalInteraction", "1.0.3")]
     public class PreventAccidentalInteraction : BaseUnityPlugin
     {
+        static readonly string defaultInteractionBlackList = "ItemStand,Sign,TeleportWorld";
+        static public ConfigEntry<string> configInteractionBlacklist { get; private set; }
+
         void Awake()
         {
+            configInteractionBlacklist = Config.Bind("General",
+                "InteractionBlacklist",
+                defaultInteractionBlackList,
+                "Classes whose interactions should be limited");
             Harmony.CreateAndPatchAll(typeof(Patches));
         }
     }
 
+    [HarmonyPatch]
     class Patches
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(ItemStand), "Interact")]
-        [HarmonyPatch(typeof(Sign), "Interact")]
-        [HarmonyPatch(typeof(TeleportWorld), "Interact")]
-        static void InteractPrefix(Humanoid __0, ref bool __result, ref bool __runOriginal, ref ItemStand __instance)
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            foreach (string s in PreventAccidentalInteraction.configInteractionBlacklist.Value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                yield return AccessTools.Method(AccessTools.TypeByName(s), "Interact");
+            }
+        }
+
+        static void Prefix(Humanoid __0, ref bool __result, ref bool __runOriginal, ref ItemStand __instance)
         {
             if (!__0.IsCrouching() && __instance.m_guardianPower == null)
             {
