dc80bc10fe440f12f70eb2a97aa68f19d2dd5587
Christopher Yeninas <phantom@sundergaming.com>
2021-03-18 00:03:35 -0400

    1.0.4
    added Nexus Update Check support
    improved compatibility with mod object interactions
    added option to toggle blocking of guardian (power) stone interactions

--- PreventAccidentalInteraction/PreventAccidentalInteraction.cs
+++ PreventAccidentalInteraction/PreventAccidentalInteraction.cs
@@ -9,18 +9,21 @@ using System.Reflection;
 
 namespace PreventAccidentalInteraction
 {
-    [BepInPlugin("com.github.phantomgamers.ValheimPreventAccidentalInteraction", "PreventAccidentalInteraction", "1.0.3")]
+    [BepInPlugin("com.github.phantomgamers.ValheimPreventAccidentalInteraction", "PreventAccidentalInteraction", "1.0.4")]
     public class PreventAccidentalInteraction : BaseUnityPlugin
     {
         static readonly string defaultInteractionBlackList = "ItemStand,Sign,TeleportWorld";
-        static public ConfigEntry<string> configInteractionBlacklist { get; private set; }
+        static public ConfigEntry<string> ConfigInteractionBlacklist { get; private set; }
+        static public ConfigEntry<bool> BlockGuardianStoneInteractions { get; private set; }
 
         void Awake()
         {
-            configInteractionBlacklist = Config.Bind("General",
+            ConfigInteractionBlacklist = Config.Bind("General",
                 "InteractionBlacklist",
                 defaultInteractionBlackList,
                 "Classes whose interactions should be limited");
+            BlockGuardianStoneInteractions = Config.Bind("General", "BlockGuardianStoneInteractions", true, "Should Guardian Stone interactions be blocked?");
+            _ = Config.Bind<int>("General", "NexusID", 161, "Nexus mod ID for updates");
             Harmony.CreateAndPatchAll(typeof(Patches));
         }
     }
@@ -30,7 +33,7 @@ namespace PreventAccidentalInteraction
     {
         static IEnumerable<MethodBase> TargetMethods()
         {
-            foreach (string s in PreventAccidentalInteraction.configInteractionBlacklist.Value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            foreach (string s in PreventAccidentalInteraction.ConfigInteractionBlacklist.Value.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
             {
                 yield return AccessTools.Method(AccessTools.TypeByName(s), "Interact");
             }
@@ -38,7 +41,7 @@ namespace PreventAccidentalInteraction
 
         static void Prefix(Humanoid __0, ref bool __result, ref bool __runOriginal, ref ItemStand __instance)
         {
-            if (!__0.IsCrouching() && __instance.m_guardianPower == null)
+            if (!__0.IsCrouching() && (!PreventAccidentalInteraction.BlockGuardianStoneInteractions.Value || __instance.m_guardianPower.m_name.IsNullOrWhiteSpace()))
             {
                 __result = false;
                 __runOriginal = false;
--- PreventAccidentalInteraction/README.md
+++ PreventAccidentalInteraction/README.md
@@ -1,17 +1,20 @@
 # Valheim-PreventAccidentalInteraction
  Prevents accidentally interacting with Signs, Item Stands, and Portal tags in Valheim by requiring that the user be crouched before allowing an interaction.
  
- # Installation
- ## Simple
+ # Installation  
+
+ ## Simple  
  Install [r2modman](https://valheim.thunderstore.io/package/ebkr/r2modman/)
  Install PreventAccidentalInteraction from https://valheim.thunderstore.io/package/PhantomGamers/PreventAccidentalInteraction/
- ## Manual
+
+ ## Manual  
  Install [BepInExPack](https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/)
  Download latest release .zip from https://github.com/PhantomGamers/Valheim-PreventAccidentalInteraction/releases/latest
  Extract PreventAccidentalInteraction.dll from downloaded zip to BepInEx/plugins and then you can launch the game.
  
- # Changes
- 1.0.3 - made prevented interactions user configurable
- 1.0.2 - don't block guardian power item stands
- 1.0.1 - added portal tag interactions
- 1.0.0 - initial release
+ # Changes  
+ 1.0.4 - added Nexus Update Check support, improved compatibility with mod object interactions
+ 1.0.3 - made prevented interactions user configurable  
+ 1.0.2 - don't block guardian power item stands  
+ 1.0.1 - added portal tag interactions  
+ 1.0.0 - initial release  
